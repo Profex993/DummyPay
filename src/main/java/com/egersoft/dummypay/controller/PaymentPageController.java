1eab21c99521bf03e2de09568d1213cd07a3eb38
Profex993 <68472681+Profex993@users.noreply.github.com>
2025-12-11 22:48:12 +0100

    UI docs added

--- src/main/java/com/egersoft/dummypay/controller/PaymentPageController.java
+++ src/main/java/com/egersoft/dummypay/controller/PaymentPageController.java
@@ -2,6 +2,10 @@ package com.egersoft.dummypay.controller;
 
 import com.egersoft.dummypay.dto.PaymentSessionUserViewDTO;
 import com.egersoft.dummypay.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Controller;
@@ -15,6 +19,22 @@ public class PaymentPageController {
 
     private final PaymentService paymentService;
 
+    @Operation(
+            summary = "Payment page",
+            description = "Renders the payment page for the given paymentId and attaches PaymentSessionUserViewDTO to the model.\nLets user preform payment or canceling.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Payment page (HTML) rendered successfully; model contains PaymentSessionUserViewDTO.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = PaymentSessionUserViewDTO.class)
+                            )
+                    ),
+                    @ApiResponse(responseCode = "404", description = "Payment not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            }
+    )
     @GetMapping("/{paymentId}")
     public String paymentPage(@PathVariable Long paymentId, Model model) {
         PaymentSessionUserViewDTO payment = paymentService.getUserPaymentSessionViewDTO(paymentId);
